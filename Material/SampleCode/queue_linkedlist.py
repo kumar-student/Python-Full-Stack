93a119eb3aa8e446599b5df01a2fb13302ab5be8
kumar <kumar>
2025-01-30 10:55:47 +0530

    Queue LinkedList

--- Material/SampleCode/queue_linkedlist.py
+++ Material/SampleCode/queue_linkedlist.py
@@ -1,66 +1,60 @@
 class Node:
     def __init__(self, value, next=None):
         self.value = value
-        self.next = None
+        self.next = next
 
 class QueueLinkedList:
-    def __init__(self, limit=10):
-        self.front_node = None
-        self.back_node = None
-        self.limit = limit
+    def __init__(self):
+        self.front = None
+        self.back = None
         self.size = 0
-
-    # def __str__(self):
-    #     current = self.front_node
-    #     if current != None:
-    #         print(current.value)
-    #         current = current.next
-    #     return super().__str__()
-    
-    # def is_full(self):
-    #     return self.size >= self.limit
-
-    # def is_empty(self):
-    #     return self.size <= 0
-    
-    # def peek(self):
-    #     if self.front_node != None:
-    #         return self.front_node.value
-    #     print("Nothing to seek")
-    #     return None
-    
-    def deque(self):
-        if self.front_node != None:
-            rmfn = self.front_node
-            self.front_node = self.front_node.next
-            print("front node: ", self.front_node.value)
-            print("back node: ", self.back_node.value)
-            print("\n")
-            return rmfn.value
-        print("Nothing to deque")
-        return None
     
     def enque(self, value):
         node = Node(value)
-        if self.back_node != None:
-            self.back_node.next = node
-            self.back_node = node
-        self.back_node = node
-        if self.front_node == None:
-            self.front_node = node
-        print("front node: ", self.front_node.value)
-        print("back node: ", self.back_node.value)
-        print("\n")
-        return node.value
+        if self.size == 0:
+            self.front = node
+            self.back = node
+            self.size += 1
+        else:
+            self.back.next = node
+            self.back = node
+            self.size += 1
+    
+    def deque(self):
+        if self.size == 0:
+            print("Queue is empty!")
+            return None
+        if self.size == 1:
+            node = self.front
+            self.front = None
+            self.back = None
+            self.size -= 1
+            return node.value
+        else:
+            node = self.front
+            self.front = node.next
+            self.size -= 1
+            return node.value
+    
+    def is_empty(self):
+        return self.size == 0
 
-q = QueueLinkedList(5)
+    def __str__(self):
+        current = self.front
+        while current != None:
+            print(current.value)
+            current = current.next
+        return super().__str__()
 
+q = QueueLinkedList()
 q.enque(10)
 q.enque(9)
 q.enque(8)
+print(q)
 
-# print(q)
-
 q.deque()
 q.deque()
 q.deque()
+print(q.is_empty())
+q.deque()
+print(q)
